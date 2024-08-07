7fd5b68ffe3fb4888680d39a4407328034288322
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-08-07 12:40:48 +0100

    feat: Add ChatMessages and NewMessage widgets to ChatScreen
    This commit adds the ChatMessages and NewMessage widgets to the ChatScreen. The ChatMessages widget displays the chat messages, while the NewMessage widget allows users to enter and send new messages. This enhancement improves the functionality of the ChatScreen by enabling users to view and send messages in the chat.

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -7,20 +9,22 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('FlutterChat'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.exit_to_app),
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text('Logged in!'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('FlutterChat'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.exit_to_app),
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+            ),
+          ],
+        ),
+        body: Column(
+          children: const [
+            Expanded(child: ChatMessages()),
+            NewMessage(),
+          ],
+        ));
   }
 }
