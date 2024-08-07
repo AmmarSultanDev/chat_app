3303abe08bf4d76d28d8b047fab69d9738b22f84
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-08-07 12:59:37 +0100

    feat: Add lazy loading for chat messages
    This commit adds lazy loading functionality to the ChatMessages widget. It retrieves chat messages from Firestore using a stream and displays them in a ListView. The messages are loaded dynamically as the user scrolls, improving the performance and user experience of the chat feature.

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,6 +6,28 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+        builder: (ctx, chatSnapShots) {
+          if (chatSnapShots.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (!chatSnapShots.hasData || chatSnapShots.data.docs.isEmpty) {
+            return const Center(
+              child: Text('No messages found.'),
+            );
+          }
+
+          final chatDocs = chatSnapShots.data!.docs;
+          return ListView.builder(
+            itemCount: chatDocs.length,
+            itemBuilder: (ctx, index) => Text(chatDocs[index]['text']),
+          );
+        });
+
     return const Center(
       child: Text('No messages found.'),
     );
